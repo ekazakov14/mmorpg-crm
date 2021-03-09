ed3cde24f139218e9642f4cd8f0f54b56697b670
eugene <eugene.kazakov78@gmail.com>
2021-03-10 01:22:23 +0300

    lint fix

--- src/commons/comparePassword.ts
+++ src/commons/comparePassword.ts
@@ -1,3 +1,6 @@
 import { compare } from 'bcrypt';
 
-export const comparePassword = (password: string, hash: string): Promise<boolean> => compare(password, hash);
+export const comparePassword = (
+  password: string,
+  hash: string,
+): Promise<boolean> => compare(password, hash);
