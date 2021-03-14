dcad2e35c776d06c0a9c1bd6a2ec358e24c853c1
eugene <eugene.kazakov78@gmail.com>
2021-03-14 14:04:21 +0300

    add ApiProperty decorator to Workspace entity

--- backend/src/modules/workspaces/entities/workspace.entity.ts
+++ backend/src/modules/workspaces/entities/workspace.entity.ts
@@ -1,16 +1,20 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from 'src/modules/users/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({
   name: 'workspaces',
 })
 export class Workspace {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   public id: number;
 
   @Column()
+  @ApiProperty()
   public name: string;
 
   @OneToMany(() => User, (user: User) => user.workspace)
+  @ApiProperty()
   public users?: User[];
 }
