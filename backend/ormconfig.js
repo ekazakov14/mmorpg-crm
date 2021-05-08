d135313e473a69e5156c2e0351e21b672551dfeb
eugene <eugene.kazakov78@gmail.com>
2021-05-08 23:00:59 +0300

    move db default connection options to ormconfig.json

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -2,18 +2,30 @@ require('dotenv').config({ path: '../.env'});
 
 module.exports = [
   {
-    name: "seed",
     type: process.env.DB_TYPE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.MYSQL_DATABASE,
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    entities: ["dist/**/*.entity{.ts,.js}"],
-    synchronize: true,
-    migrations: ["dist/src/db/seeds/index.js"],
+    entities: [__dirname + '/dist/src/**/*.entity.{ts,js}'],
+    migrations: ['dist/src/db/migrations/*.{ts,js}'],
     cli: {
-      "migrationsDir": "src/db/seeds"
-    }
+      'migrationsDir': 'src/db/migrations',
+    },
+  },
+  {
+    name: 'seed',
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.MYSQL_DATABASE,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    entities: [__dirname + '/dist/src/**/*.entity.{ts,js}'],
+    migrations: ['dist/src/db/seeds/index.js'],
+    cli: {
+      'migrationsDir': 'src/db/seeds',
+    },
   }
 ]
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,32 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { WorkspacesModule } from './modules/workspaces/workspaces.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '../.env' }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        const options: TypeOrmModuleOptions = {
-          type: config.get<any>('DB_TYPE'),
-          host: config.get<any>('DB_HOST'),
-          port: config.get<any>('DB_PORT'),
-          database: config.get<any>('MYSQL_DATABASE'),
-          username: config.get<any>('MYSQL_USER'),
-          password: config.get<any>('MYSQL_PASSWORD'),
-          entities: [__dirname + '/**/*.entity.{ts,js}'],
-          autoLoadEntities: true,
-          synchronize: true,
-        };
-
-        return options;
-      },
-    }),
+    TypeOrmModule.forRoot(),
     UsersModule,
     AuthModule,
     WorkspacesModule,
